072d8fd4dfb45a81660fce036ff57d3fa229525c
3NinjasFun <739237@student.dusd.net>
2023-03-23 09:59:01 -0700

    Up to 3.2 part 4

---
+++ Unit3SanchezP1/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrerfab;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+
+    float startDelay = 2;
+    float repeatRate = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnObstacle ()
+    {
+        Instantiate(obstaclePrerfab, spawnPos, obstaclePrerfab.transform.rotation);
+    }
+}
+
