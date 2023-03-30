7a9ac643e8cf2c98b8d79a2cf49560895fb38c75
3NinjasFun <739237@student.dusd.net>
2023-03-29 21:58:49 -0700

    Finished 3.4
    Also bug fixed the problem with moving forward and falling over after a couple jumps

--- Unit3SanchezP1/Assets/Scripts/PlayerControl.cs
+++ Unit3SanchezP1/Assets/Scripts/PlayerControl.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerControl : MonoBehaviour
 {
@@ -10,25 +11,42 @@ public class PlayerControl : MonoBehaviour
     public bool isOnGround = true;
     public bool gameOver = false;
     private Animator playerAnim;
-
+    public ParticleSystem explosionParticale;
+    public ParticleSystem dirtParticale;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    private AudioSource playerAudio;
+    public float jumpBorder;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
-        
+        playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
+            dirtParticale.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+        }
+
+        if (transform.position.x > jumpBorder)
+        {
+            transform.position = new Vector3(jumpBorder, transform.position.y , transform.position.z);
+            
+        }
+        if (isOnGround)
+        {
+            transform.rotation = Quaternion.Euler(0, 90, 0);
         }
         
     }
@@ -37,12 +55,19 @@ public class PlayerControl : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            dirtParticale.Play();
+            
         }
 
         else if (collision.gameObject.CompareTag("Obstacle"))
         {  
             gameOver = true;
             Debug.Log("Game Over");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticale.Play();
+            dirtParticale.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
         }
     }
 }
